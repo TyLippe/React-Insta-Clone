ef766e5d5cdced341e15c5e0b9434280dd42d962
Ty Lippe <tylippe94@gmail.com>
2019-06-05 14:51:22 -0400

    post container running

---
+++ instagram-app/src/components/CommentSection/CommentSection.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class CommentSection extends React.Component {
+    render() {
+        return (
+            <div>comments here</div>
+        )
+    }
+}
+
+export default CommentSection;
